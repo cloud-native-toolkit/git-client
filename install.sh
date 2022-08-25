04d7952bae833fbcdf1471894b2f448eb391323a
seansund <seansund@users.noreply.github.com>
2022-08-25 18:27:03 +0000

    deploy: 8156b0417caa9146655a5cdcabe8c640fb3fdae0

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+DEST_DIR="${1:-$DEST_DIR}"
+RELEASE="${2:-$RELEASE}"
+
+if [ -z "${DEST_DIR}" ]; then
+  DEST_DIR="/usr/local/bin"
+fi
+
+if ! command -v curl 1> /dev/null 2> /dev/null; then
+  echo "curl cli not found" >&2
+  exit 1
+fi
+
+if [ -z "${RELEASE}" ]; then
+  RELEASE=$(curl -sL "https://api.github.com/repos/cloud-native-toolkit/git-client/releases/latest" | grep tag_name | sed 's/"tag_name"//g' | sed 's/ //g' | sed 's/://g' | sed 's/,//g' | sed 's/"//g')
+fi
+
+TYPE="linux"
+OS=$(uname)
+if [ "$OS" = "Linux" ]; then
+  TYPE=$(cat /etc/os-release | grep -E "^ID=" | sed "s/ID=//g")
+  if [ "${TYPE}" != "alpine" ]; then
+    TYPE="linux"
+  fi
+elif [ "$OS" = "Darwin" ]; then
+  TYPE="macos"
+else
+  echo "OS not supported"
+  exit 1
+fi
+
+ARCH=""
+case $(uname -m) in
+    i386)    ARCH="x64" ;;
+    i686)    ARCH="x64" ;;
+    x86_64)  ARCH="x64" ;;
+    aarch64) ARCH="arm64" ;;
+    arm64)   ARCH="arm64" ;;
+    *)       echo "Unable to determine system architecture" >&2; exit 1 ;;
+esac
+
+echo "Installing version ${RELEASE} of gitu for ${TYPE}-${ARCH} into ${DEST_DIR}"
+curl --progress-bar -Lo "${DEST_DIR}/gitu" "https://github.com/cloud-native-toolkit/git-client/releases/download/${RELEASE}/gitu-${TYPE}-${ARCH}" && chmod +x "${DEST_DIR}/gitu"
