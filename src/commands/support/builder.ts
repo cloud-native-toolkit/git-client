8edfc9b980560b3db706523c8b3af988e94ceef3
Sean Sundberg <seansund@us.ibm.com>
2022-08-25 12:53:40 -0500

    Allows caCert file name to be provided in GIT_CA_CERT env variable
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/commands/support/builder.ts
+++ src/commands/support/builder.ts
@@ -1,14 +1,15 @@
 import {Argv} from 'yargs';
 import {forCaCertFile} from './checks';
-import {caCertAbsolutePath} from './middleware';
+import {caCertAbsolutePath, loadFromEnv} from './middleware';
 
 export const defaultBuilder = (yargs: Argv<any>) => {
   return yargs
     .option('caCert', {
       type: 'string',
-      description: 'Name of the file containing the ca certificate for SSL connections',
+      description: 'Name of the file containing the ca certificate for SSL connections. The value can also be provided in the `GIT_CA_CERT` environment variable.',
       demandOption: false
     })
+    .middleware(loadFromEnv('caCert', 'GIT_CA_CERT'), true)
     .middleware(caCertAbsolutePath())
     .check(forCaCertFile())
 }
